639347a64c11b8105160b3cf5d69117d4292ee40
Pete Du Beau <peterdubeau@yahoo.com>
2020-08-25 13:32:54 -0400

    json set up and project cards created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Nav from './Nav/Nav'
+import Projects from './Projects/Projects'
 
 function App() {
   return (<>
-   <Nav />
-  </>);
+    <Nav />
+    <Projects />
+      </>);
 }
 
 export default App;
