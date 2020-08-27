1d8c31e88dc67adf5c3a77dbb2e842133fbc15eb
Pete Du Beau <peterdubeau@yahoo.com>
2020-08-27 13:54:30 -0400

    minor bug-fixes

--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -5,7 +5,7 @@ export default function Intro() {
   return (
     <div className="image">
       <h2 className ='text'>
-        Hey! I'm Pete Du Beau, a Solutions Engineer and Full Stack Web Developer! 
+        Hey, I'm Pete DuBeau, a Solutions Engineer and Full Stack Web Developer.
         <p className ="check" ><a href="#card-list">Check out my projects!</a></p>
       </h2>
     </div>
