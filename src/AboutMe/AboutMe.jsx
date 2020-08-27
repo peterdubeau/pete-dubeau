099981654ff2c3c5fe8559b2335bf901e6ed2603
Pete Du Beau <peterdubeau@yahoo.com>
2020-08-27 11:52:16 -0400

    media queries complete

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Skills from '../Skills/Skills'
 import './AboutMe.css'
 
 export default function AboutMe() {
@@ -7,13 +8,14 @@ export default function AboutMe() {
       <article className="project-card" id="about">
           <h2 className= "about">About Me</h2>
           <p className="me">
-            I am a software engineer who leverages 8 years of sales and account management 
-            experience to build and implement consumer driven products.
-            Whether it be navigating B2B sales issues or developing websites,
-            I am an expert at diagnosing the core problems to technical issues and
-            developing customized solutions that speak to the heart of customers needs.
-            I seek the opportunity to join a forward-thinking technology team that places
+          I am a software engineer who leverages 8 years of sales and account management 
+          experience to build and implement consumer driven products.
+          Whether it be navigating B2B sales issues or developing websites,
+          I am an expert at diagnosing the core problems to technical issues and
+          developing customized solutions that speak to the heart of customers needs.
+          I seek the opportunity to join a forward-thinking technology team that places
             consumer experience at the center of what they do. </p>
+            <Skills />
       </article>
     </div>
   )
---
+++ src/Skills/Skills.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Skills.css'
+
+export default function Skills() {
+  return (
+    <div className="skills">
+      <img className="skill-icon" id="ruby" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598456860/portfolio/tool-icons/ruby_vn8zlo.png" />
+      <img className="skill-icon" id="css" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598456860/portfolio/tool-icons/css_o6imgu.png" />
+      <img className="skill-icon" id="html" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598456860/portfolio/tool-icons/htmlicon_nmiauu.png" />
+      <img className="skill-icon" id="npm" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598456860/portfolio/tool-icons/npmicon_wmoxji.svg" />
+      <img className="skill-icon" id="nodeJs" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598456860/portfolio/tool-icons/nodejs_watk47.png" />
+      <img className="skill-icon" id="react" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598456860/portfolio/tool-icons/react_icon_sun4xy.png" />
+      <img className="skill-icon" id="mongo" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598542145/portfolio/tool-icons/mongo_skmait.png" />
+      <img className="skill-icon" id="heroku" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598542145/portfolio/tool-icons/Heroku_wuefee.png" />
+      <img className="skill-icon" id="git" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598542145/portfolio/tool-icons/git_syuihb.png" />
+      <img className="skill-icon" id="github" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598542145/portfolio/tool-icons/github_l8jzfj.png" />
+    </div>
+  )
+}
