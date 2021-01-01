2b36bc9c9a15164164bfa56f576f22d023986ca6
Pete Du Beau <peterdubeau@yahoo.com>
2021-01-01 11:39:38 -0500

    retouched

--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -4,10 +4,12 @@ import './Intro.css'
 export default function Intro() {
   return (
     <div className="image">
-      <h2 className ='text'>
-        Hey, I'm Pete DuBeau, a Solutions Engineer and Full Stack Web Developer.
-        <p className ="check" ><a href="#card-list">Check out my projects!</a></p>
-      </h2>
+      <div className='text'>
+        <h2 className ='intro'>
+          Hey, I'm Pete DuBeau, a Solutions Engineer and Full Stack Web Developer.
+        </h2>
+          <p className ="check" ><a href="#card-list">Check out my projects!</a></p>
+      </div>
     </div>
   )
 }
