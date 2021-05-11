0378c549ba4b273f364aff39f21c92d73e1912cf
Pete Du Beau <peterdubeau@yahoo.com>
2021-05-10 20:12:56 -0400

    intro looks good. Media queries need to be cleaned up for moblie

--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -4,12 +4,10 @@ import './Intro.css'
 export default function Intro() {
   return (
     <div className="image">
-      <div>
       <h2 className ='text'>
         Hey, I'm Pete DuBeau, a Solutions Engineer and Full Stack Web Developer.
         <p className ="check" ><a href="#card-list">Check out my projects!</a></p>
-        </h2>
-      </div>
+      </h2>
     </div>
   )
 }
