0def7ad75f4729979476f461c694b0076f95ff8e
Pete Du Beau <peterdubeau@yahoo.com>
2021-05-14 10:40:44 -0400

    cleaned up mobile bars

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -4,7 +4,8 @@ import "./AboutMe.css";
 
 export default function AboutMe() {
   return (
-    <div className="about-container"  id="about">
+    <div className="about-container" id="about">
+      <div className="black-strip-about"></div>
       <div className="about-text">
       <h2 className="about">Technologies used</h2>
         <p>I'm skilled in modern web development including JavaScript, React, and Ruby on Rails, Just to name a few.</p>
